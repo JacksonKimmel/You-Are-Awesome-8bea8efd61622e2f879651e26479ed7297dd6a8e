446aa59f05d663f24584869398889d9d83c369a4
Jackson Kimmel <jkimmel2@zagmail.gonzaga.edu>
2022-01-18 19:59:06 -0800

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Jackson Kimmel on 1/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("👍 viewDidLoad has run!")
+        messageLabel.text = "Fabulous? That's You!"
+    }
+
+
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("😎 The message button was pressed!")
+        messageLabel.text = "You Are Awesome!"
+    }
+}
+
