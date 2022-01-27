f5c1e97a3869d437c784ee58b3464a3315d21ccd
Jackson Kimmel <jkimmel2@zagmail.gonzaga.edu>
2022-01-27 11:40:36 -0800

    finished you are awesome app

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -11,6 +11,7 @@ import AVFoundation
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var playSoundSwitch: UISwitch!
     
     var imageNumber = -1
     var messageNumber = -1
@@ -58,8 +59,17 @@ class ViewController: UIViewController {
         imageView.image = UIImage(named: "image\(imageNumber)")
         
         soundNumber = nonRepeatingRandom(originalNumber: soundNumber, upperLimit: totalNumberOfSounds-1)
+        if playSoundSwitch.isOn {
          playSound(name: "sound\(soundNumber)")
 
     }
+    }
+    @IBAction func playSoundToggled(_ sender: UISwitch) {
+        if !sender.isOn && audioPlayer != nil {
+            audioPlayer.stop()
+            
+        }
+    }
 }
+
 
